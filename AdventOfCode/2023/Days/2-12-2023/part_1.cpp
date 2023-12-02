ee343ccc393210d63caa2d899a2ffb38837bda29
Emka5885 <masiak-emilia@wp.pl>
2023-12-02 11:15:39 +0100

    Aoc - 2.12.2023   part 1

---
+++ AdventOfCode/2023/Days/2-12-2023/part_1.cpp
@@ -0,0 +1,85 @@
+/*
+#include <iostream>
+#include <fstream>
+#include <string>
+
+bool IsDigit(char c)
+{
+	return c >= '0' && c <= '9';
+}
+
+void CheckNumberOfCubes(std::string& set, const std::string& searched, int& max)
+{
+	int id = set.find(searched);
+	if (id != -1)
+	{
+		std::string number = "";
+
+		int i = 0;
+		while (true)
+		{
+			if (id - 2 - i < 0)
+				break;
+
+			if (!IsDigit(set[id - 2 - i]))
+				break;
+
+			number += set[id - 2 - i];
+			++i;
+		}
+		std::reverse(number.begin(), number.end());
+
+		int numOfCubes = std::stoi(number);
+
+		if (max < numOfCubes)
+			max = numOfCubes;
+	}	
+}
+
+bool IsTheGamePossible(std::string& game)
+{
+	int maxRed, maxBlue, maxGreen;
+	maxRed = maxBlue = maxGreen = 0;
+
+	while (true)
+	{
+		std::string set = game.substr(0, game.find(";"));
+
+		CheckNumberOfCubes(set, "red", maxRed);
+		CheckNumberOfCubes(set, "blue", maxBlue);
+		CheckNumberOfCubes(set, "green", maxGreen);
+
+		if (game.find(";") == -1)
+			break;
+
+		game.erase(0, game.find(";") + 1);
+	}
+
+	return maxRed <= 12 && maxGreen <= 13 && maxBlue <= 14;
+}
+
+int main()
+{
+	std::fstream file("AdventOfCode//2023//File_txt//02-12-2023.txt");
+
+	int index = 1;
+	int result = 0;
+
+	while (!file.eof())
+	{
+		std::string line;
+		std::getline(file, line);
+		if (IsTheGamePossible(line))
+		{
+			result += index;
+		}
+
+		index++;
+	}
+	file.close();
+
+	std::cout << result << "\n";
+
+	return 0;
+}
+*/
